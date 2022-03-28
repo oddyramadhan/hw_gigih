7f8c2e12c4d26edadf2591a4f3acfc1cdf8a6dd3
oddyramadhan <ramadhan528@gmail.com>
2022-03-28 14:48:12 +0700

    homework module 2 session 3

--- src/App.js
+++ src/App.js
@@ -3,33 +3,33 @@ import "./App.css";
 import Albums from "./components/Albums";
 
 function Playlist() {
-  const [data, setData] = useState({});
+  // const [data, setData] = useState({});
 
-  useEffect(() => {
-    const getDataAndRender = async () => {
-      try {
-        const response = await fetch(
-          "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json"
-        );
-        if (!response.ok) throw new Error("Error");
-        const results = await response.json();
-        console.log("using async", results);
-        setData(results);
-      } catch (error) {
-        console.log(error);
-        alert(error);
-      }
-    };
-    getDataAndRender();
-  }, []);
+  // useEffect(() => {
+  //   const getDataAndRender = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json"
+  //       );
+  //       if (!response.ok) throw new Error("Error");
+  //       const results = await response.json();
+  //       console.log("using async", results);
+  //       setData(results);
+  //     } catch (error) {
+  //       console.log(error);
+  //       alert(error);
+  //     }
+  //   };
+  //   getDataAndRender();
+  // }, []);
 
-  if (data.album === undefined) {
-    return <h2>Loading..</h2>;
-  }
+  // if (data.album === undefined) {
+  //   return <h2>Loading..</h2>;
+  // }
 
   return (
     <div className="wrapper">
-      <Albums data={data} />
+      <Albums />
     </div>
   );
 }
--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,31 +1,45 @@
 import React from "react";
 import Tracks from "./Tracks";
+import data from "../Data";
 
-function Albums({ data }) {
+function Albums() {
   return (
-    <div className="container">
-      <div className="box">
-        <div className="playing">
-          <img className="logo" alt="album" src={data.album.images[0].url} />
-          <div className="list">
-            <p>{data.album.type}</p>
-            <p className="album">{data.album.name}</p>
-            <p>{data.album.artists[0].name}</p>
+    <div className="kotak">
+      {data.map((v, index) => (
+        <div className="container" key={index}>
+          <div className="box">
+            <div className="playing">
+              <img className="logo" alt="album" src={v.album.images[0].url} />
+              <div className="list">
+                <p className="album-type">{v.album.album_type}</p>
+                <p className="name">{v.album.name}</p>
+                <p>{v.album.artists[0].name}</p>
+              </div>
+              <div className="btn">
+                <button>
+                  <img
+                    src="play.logo.png"
+                    height="20px"
+                    width="30px"
+                    alt="play"
+                  />
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="btn">
-            <button>
-              <img src="play.logo.png" height="20px" width="30px" alt="play" />
-            </button>
+          <div className="playlist-song">
+            <div className="head">
+              <p>TITLE</p>
+              <p>DURATION</p>
+            </div>
+            <ol>
+              <li>
+                <Tracks data={v} />
+              </li>
+            </ol>
           </div>
         </div>
-      </div>
-      <div className="playlist-song">
-        <div className="head">
-          <p>TITLE</p>
-          <p>DURATION</p>
-        </div>
-        <Tracks data={data} />
-      </div>
+      ))}
     </div>
   );
 }
