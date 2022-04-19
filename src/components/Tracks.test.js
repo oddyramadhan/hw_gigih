d92f886f3be2bea1694a7d9db24cd15d55c47ad3
oddyramadhan <ramadhan528@gmail.com>
2022-04-20 00:57:21 +0700

    test track component

--- src/components/Tracks.test.js
+++ src/components/Tracks.test.js
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import Tracks from "./Tracks";
-import store from "../../redux/store";
+import store from "../pages/reducers/store";
 
 test("render tracks component", () => {
   render(
